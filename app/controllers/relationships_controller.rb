36ae08b32e389fbdb314803ac6c90d1962d4fb83
EC2 Default User <ec2-user@ip-172-31-92-183.ec2.internal>
2022-02-28 07:50:46 +0000

    フォロー・フォロワー機能

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,23 @@
+class RelationshipsController < ApplicationController
+  def create
+    unless current_user == User.find(params[:user_id])
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+    end
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+end
